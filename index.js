d62c44a01543c63b6e90a0aec3923bd72fe96894
chris13012 <christophecaillet@icloud.com>
2019-07-09 16:48:17 +0200

    errors handling

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const app = express();
+const Joi = require("joi");
 
 const genres = [
   {
@@ -11,10 +12,17 @@ const genres = [
     name: "action"
   },
   {
-    id: 0,
+    id: 2,
     name: "romance"
   }
 ];
+
+const schema = Joi.object().keys({
+  name: Joi.string()
+    .min(3)
+    .max(30)
+    .required()
+});
 
 app.use(express.json());
 
@@ -24,10 +32,14 @@ app.get("/api/genres", (req, res) => {
 
 app.get("/api/genres/:id", (req, res) => {
   const genre = genres.find(g => g.id == req.params.id);
+  if (!genre) return res.status(404).send("Ce genre n'existe pas");
   res.send(genre);
 });
 
 app.post("/api/genres", (req, res) => {
+  const { error } = validateGenre(req.body);
+  if (error) return res.status(400).send(error.details[0].message);
+
   const genre = {
     id: genres.length + 1,
     name: req.body.name
@@ -36,19 +48,31 @@ app.post("/api/genres", (req, res) => {
   res.send(genre);
 });
 
-app.put("/api/genres", (req, res) => {
-  const genre = genres.find(g => g.id === req.body.id);
+app.put("/api/genres/:id", (req, res) => {
+  const { error } = validateGenre(req.body);
+  if (error) return res.status(400).send(error.details[0].message);
+
+  const genre = genres.find(g => g.id == req.params.id);
+  if (!genre) return res.status(404).send("Ce genre n'existe pas");
+
   genre.name = req.body.name;
-  const index = genres.findIndex(g => g.name === req.body.name);
+  const index = genres.findIndex(g => g.name == req.body.name);
   genres[index] = genre;
   res.send(genre);
+  actio;
 });
 
-app.delete("/api/genres", (req, res) => {
-  const index = genres.findIndex(g => g.id === req.body.id);
+app.delete("/api/genres/:id", (req, res) => {
+  const index = genres.findIndex(g => g.id == req.params.id);
+  if (!index) return res.status(404).send("Ce genre n'existe pas");
   genres.splice(index, 1);
   res.send(genres);
 });
+
+function validateGenre(obj) {
+  const result = Joi.validate(obj, schema);
+  return result;
+}
 
 const port = process.env.PORT || 3000;
 
