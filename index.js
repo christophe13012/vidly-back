51bf42bf2a1ddd6548f850449342205048dec3f1
chris13012 <christophecaillet@icloud.com>
2019-07-09 15:47:48 +0200

    routes

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const app = express();
+const bodyParser = require("body-parser");
 
 const genres = [
   {
@@ -16,7 +17,38 @@ const genres = [
   }
 ];
 
-app.get("/api/genres", function(req, res) {
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+app.get("/api/genres", (req, res) => {
+  res.send(genres);
+});
+
+app.get("/api/genres/:id", (req, res) => {
+  const genre = genres.find(g => g.id == req.params.id);
+  res.send(genre);
+});
+
+app.post("/api/genres", (req, res) => {
+  const genre = {
+    id: genres.length + 1,
+    name: req.body.name
+  };
+  genres.push(genre);
+  res.send(genre);
+});
+
+app.put("/api/genres", (req, res) => {
+  const genre = genres.find(g => g.id === req.body.id);
+  genre.name = req.body.name;
+  const index = genres.findIndex(g => g.name === req.body.name);
+  genres[index] = genre;
+  res.send(genre);
+});
+
+app.delete("/api/genres", (req, res) => {
+  const index = genres.findIndex(g => g.id === req.body.id);
+  genres.splice(index, 1);
   res.send(genres);
 });
 
