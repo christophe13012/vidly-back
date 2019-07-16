1f7a2ff4b0d3fbb7675d9ec2d5bc1a382a672a47
chris13012 <christophecaillet@icloud.com>
2019-07-16 16:59:17 +0200

    cors added

--- index.js
+++ index.js
@@ -7,8 +7,17 @@ const { customers } = require("./routes/customers");
 const { movies } = require("./routes/movies");
 const rentals = require("./routes/rentals");
 
-mongoose.connect("mongodb://localhost/vidly", { useNewUrlParser: true }, () => {
+mongoose.connect("mongodb://localhost/vidly", () => {
   console.log("Connected to db");
+});
+
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  next();
 });
 
 app.use(express.json());
