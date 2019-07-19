140e3158aa79e6d1ccddb15f2b6b316719e0241c
chris13012 <christophecaillet@icloud.com>
2019-07-19 17:00:04 +0200

    mongo

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const { movies } = require("./routes/movies");
 const rentals = require("./routes/rentals");
 
 mongoose.connect(
-  "mongodb+srv://chris:<test>@vidly-qu0jp.gcp.mongodb.net/test?retryWrites=true&w=majority/vidly",
+  "mongodb+srv://chris:<test>@vidly-qu0jp.gcp.mongodb.net/test?retryWrites=true&w=majority",
   () => {
     console.log("Connected to db");
   }
